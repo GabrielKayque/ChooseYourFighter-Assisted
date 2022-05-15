e5e83803503391f7fa8972ae3a3f690bc39a8471
GabrielKayque <mr.biiiel@hotmail.com>
2022-05-15 01:10:36 -0300

    dynamic name

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ characters.forEach((character) => {
     character.addEventListener('mouseenter', () => {
         document.querySelector('.selected').classList.remove('selected');
         character.classList.add('selected');
+
         // Change the name e photo from big character 1 when mouse is hovering list
-        
         const imgPlayer1 = document.getElementById('character-player1');
         // console.log(imgPlayer1)
         const imgSelected = character.children[1].dataset.png;
         // console.log(imgSelected)
         imgPlayer1.attributes.src.value = `src/images/${imgSelected}`;
         console.log(imgPlayer1.attributes.src.value);
-    });
-});
 
+        document.querySelector('#character-name').innerHTML = character.dataset.name
+    });
+})
 
