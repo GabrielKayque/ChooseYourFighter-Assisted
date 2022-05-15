f0c52abc934bd4f2ed1013c14262a6231d9c9d69
GabrielKayque <mr.biiiel@hotmail.com>
2022-05-14 23:54:53 -0300

    hover character update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 // Change the ul > li class to selected  when mouse is hovering
 
+const characters = document.querySelectorAll('.character-list>li')
+
+characters.forEach((character) => {
+    character.addEventListener('mouseenter', () => {
+        document.querySelector('.selected').classList.remove('selected')
+        character.classList.add('selected')
+    })
+})
+
 // Change the name e photo from big character 1 when mouse is hovering
