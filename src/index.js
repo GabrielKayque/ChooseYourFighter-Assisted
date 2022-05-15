76f6e719577f42ec806942661a0239ea26c87a4e
GabrielKayque <mr.biiiel@hotmail.com>
2022-05-15 01:02:40 -0300

    dynamic player1 when char selecting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 // Change the ul > li class to selected  when mouse is hovering
 
-const characters = document.querySelectorAll('.character-list>li')
+const characters = document.querySelectorAll('.character-list>li');
 
 characters.forEach((character) => {
     character.addEventListener('mouseenter', () => {
-        document.querySelector('.selected').classList.remove('selected')
-        character.classList.add('selected')
-    })
-})
+        document.querySelector('.selected').classList.remove('selected');
+        character.classList.add('selected');
+        // Change the name e photo from big character 1 when mouse is hovering list
+        
+        const imgPlayer1 = document.getElementById('character-player1');
+        // console.log(imgPlayer1)
+        const imgSelected = character.children[1].dataset.png;
+        // console.log(imgSelected)
+        imgPlayer1.attributes.src.value = `src/images/${imgSelected}`;
+        console.log(imgPlayer1.attributes.src.value);
+    });
+});
 
-// Change the name e photo from big character 1 when mouse is hovering
+
